7857d83b39558aa12f612ef2db83ad636219b377
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-12-08 18:32:05 +0100

    Build ThemePicker
    Change-Id: I6c2acaa6b33ad8937e481d2e9675a5b1eecd23cc
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- main.mk
+++ main.mk
@@ -4,4 +4,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 # Boot Animation
 include vendor/themes/bootanimation/bootanimation.mk
 
+# Packages
+include vendor/themes/packages/packages.mk
 
